69b9738265d607c39f255577aded7f34c6e52c3b
Marcelin97 <lois_m@outlook.com>
2023-03-03 15:16:50 +0100

    change menu mobile to visibility propertie

--- app/js/script.js
+++ app/js/script.js
@@ -3,10 +3,17 @@ const btnHambuger = document.querySelector("#btnHamburger");
 
 const header = document.querySelector('.header')
 
+const overlay = document.querySelector('.overlay');
+
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     header.classList.remove("open");
+    overlay.classList.add('fade-out')
+    overlay.classList.remove('fade-in')
   } else {
+    // Open hamburger menu
     header.classList.add("open");
+    overlay.classList.add('fade-in')
+    overlay.classList.remove('fade-out')
   }
 });
