6ad0956f5fa20780fbe31a3baa18136cfca117df
Marcelin97 <lois_m@outlook.com>
2023-03-09 11:00:58 +0100

    test

--- app/js/script.js
+++ app/js/script.js
@@ -5,15 +5,28 @@ const header = document.querySelector('.header')
 
 const overlay = document.querySelector('.overlay');
 
+const fadeElems = document.querySelectorAll('.has-fade')
+
+const body = document.querySelector('body');
+
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
+    body.classList.remove('noscroll');
     header.classList.remove("open");
-    overlay.classList.add('fade-out')
-    overlay.classList.remove('fade-in')
+    fadeElems.forEach(function(element){
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+      });
   } else {
+    // lock scroll when open the menu
+    body.classList.add('noscroll');
+
     // Open hamburger menu
     header.classList.add("open");
-    overlay.classList.add('fade-in')
-    overlay.classList.remove('fade-out')
+    fadeElems.forEach(function(element){
+        element.classList.add('fade-in')
+        element.classList.remove('fade-out')
+    })
+
   }
 });
